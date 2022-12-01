c7470db0f060f39ba8229d0b5eeb1b4f90685d10
Mantas <mantas.gulbinskas@gmail.com>
2022-12-01 18:08:02 +0200

    added hero card

--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BlackLine } from "../components/BlackLine/BlackLine";
 import "../components/Hero/background.css";
 import Car from "../components/Hero/Car";
+import {HeroCard} from "../components/Hero/HeroCard";
 
 export const Hero = () => {
   return (
     <div>
       <BlackLine />
       <section className="flex md:flex-row flex-col bg-primary sm:py-16 py-6 background">
+          <HeroCard />
         <Car />
       </section>
     </div>
